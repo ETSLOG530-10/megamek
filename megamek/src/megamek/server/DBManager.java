2d80d7d89608af1fbe12aef31f3819d260236132
Simon Legros <simonlegros5@gmail.com>
2022-04-06 09:30:09 -0400

    Add elo getter methods in DBManager

--- megamek/src/megamek/server/DBManager.java
+++ megamek/src/megamek/server/DBManager.java
@@ -4,12 +4,15 @@ import megamek.MegaMek;
 import org.apache.logging.log4j.LogManager;
 
 import java.sql.*;
+import java.util.HashMap;
 
 public class DBManager {
 
     public static void initiateDB() {
         createEloTable();
-        insertIntoTable("Simon", 2);
+        addOrUpdateRow("Simon", 2);
+        //getAll();
+        getEloByUsername("Simon");
     }
 
     private static Connection connect() {
@@ -57,7 +60,7 @@ public class DBManager {
         }
     }
 
-    private static void insertIntoTable(String username, int elo) {
+    private static void addOrUpdateRow(String username, int elo) {
         Connection c = null;
         Statement stmt = null;
 
@@ -74,11 +77,67 @@ public class DBManager {
             c.commit();
             c.close();
         } catch ( Exception e ) {
-            MegaMek.printToOut(e.getMessage());
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
             System.exit(0);
         }
-        MegaMek.printToOut("Records created successfully");
         System.out.println("Records created successfully");
+    }
+
+    public static HashMap<String, Integer> getAll() {
+        Connection c = null;
+        Statement stmt = null;
+
+        try {
+            c = connect();
+            c.setAutoCommit(false);
+
+            stmt = c.createStatement();
+            String sql = "SELECT * FROM elo_score ORDER BY USERNAME;";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            HashMap<String, Integer> eloMap = new HashMap();
+            while ( rs.next() ) {
+                String  username = rs.getString("USERNAME");
+                int elo = rs.getInt("ELO");
+                eloMap.put(username, elo);
+            }
+            rs.close();
+            stmt.close();
+            c.close();
+            return eloMap;
+
+        } catch ( Exception e ) {
+            MegaMek.printToOut( e.getClass().getName() + ": " + e.getMessage() );
+        }
+        return null;
+    }
+
+    public static int getEloByUsername(String name) {
+        Connection c = null;
+        Statement stmt = null;
+
+        try {
+            c = connect();
+            c.setAutoCommit(false);
+
+            stmt = c.createStatement();
+            String sql = "SELECT * FROM elo_score WHERE USERNAME="+name+";";
+            ResultSet rs = stmt.executeQuery(sql);
+            int elo = 0;
+            if ( rs.first() ) {
+                String  username = rs.getString("USERNAME");
+                elo = rs.getInt("ELO");
+                MegaMek.printToOut("USERNAME = " + username);
+            }
+            rs.close();
+            stmt.close();
+            c.close();
+
+            return elo;
+
+        } catch ( Exception e ) {
+            MegaMek.printToOut( e.getClass().getName() + ": " + e.getMessage() );
+        }
+        return 0;
     }
 }
